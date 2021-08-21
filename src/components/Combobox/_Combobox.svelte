6034975278657f769413ec2328b0cff11286cce8
haruaki07 <misbahulk2236@gmail.com>
2021-08-21 13:32:58 +0700

    add combobox/select component

---
+++ src/components/Combobox/_Combobox.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Select from "svelte-select";
+
+  export let options: string[];
+  export let value = "";
+  export let label = "";
+</script>
+
+<!-- svelte-ignore a11y-label-has-associated-control -->
+<label class="combobox">
+  {#if label}
+    <span class="combobox-label">{label}</span>
+  {/if}
+  <Select on:select items={options} {value} {...$$restProps} />
+</label>
+
+<style lang="postcss">
+  .combobox {
+    --border: theme("colors.gray.500") 1px solid;
+
+    @apply block w-full;
+  }
+
+  .combobox-label {
+    @apply font-semibold text-sm block mb-1;
+  }
+</style>
--- src/pages/index/App.svelte
+++ src/pages/index/App.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
-  import Textbox from "@/components/Textbox.svelte";
+  import Textbox from "@/components/Textbox";
   import Toggle from "@/components/Toggle";
+  import Combobox from "@/components/Combobox";
 </script>
 
 <div class="p-8 space-y-4">
   <Textbox type="text" placeholder="Name" label="Textbox" />
   <Toggle offLabel="Off" onLabel="On" label="Status" />
+  <Combobox
+    label="Combobox"
+    placeholder="Select.."
+    options={["Apple", "Orange", "Pineapple"]}
+    value="Orange"
+  />
 </div>
