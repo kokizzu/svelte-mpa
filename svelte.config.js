d4816d5bb68766571724c87414818f0d246ed638
Kiswono Prayogo <kiswono@gmail.com>
2022-05-31 00:58:02 +0700

    remove five-server to dev, add svelte to dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
 const sveltePreprocess = require('svelte-preprocess');
-const { windi } = require('svelte-windicss-preprocess');
-const { writeFileSync } = require('fs');
 
 const contentReplacer = args => global.zPlaceholderReplacer && { code: global.zPlaceholderReplacer(args.content) };
 
